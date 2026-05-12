4c1f66bf71abdaae22c3320fdcd4fa30b6982442
nishi2006chauhan <nishi2006chauhan@gmail.com>
2026-05-12 15:39:42 +0530

    implemented about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./Component/Navbar.jsx";
 import Hero from "./Component/Hero.jsx";
 import Cocktails from "./Component/Cocktails.jsx";
+import About from "./Component/About.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +14,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
--- src/Component/Cocktails.jsx
+++ src/Component/Cocktails.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { cocktailLists, mockTailLists } from "../../Constants/index.js";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { cocktailLists, mockTailLists } from "../../Constants/index.js";
 
 const Cocktails = () => {
   useGSAP(() => {
-    const parallaxtimeline = gsap.timeline({
+    const parallaxTimeline = gsap.timeline({
       scrollTrigger: {
         trigger: "#cocktails",
-        start: "top 20%",
+        start: "top 30%",
         end: "bottom 80%",
         scrub: true,
       },
     });
-    parallaxtimeline
+
+    parallaxTimeline
       .from("#c-left-leaf", {
         x: -100,
         y: 100,
@@ -23,6 +23,7 @@ const Cocktails = () => {
         y: 100,
       });
   });
+
   return (
     <section id="cocktails" className="noisy">
       <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
@@ -31,9 +32,11 @@ const Cocktails = () => {
         alt="r-leaf"
         id="c-right-leaf"
       />
+
       <div className="list">
         <div className="popular">
           <h2>Most popular cocktails:</h2>
+
           <ul>
             {cocktailLists.map(({ name, country, detail, price }) => (
               <li key={name}>
@@ -43,13 +46,15 @@ const Cocktails = () => {
                     {country} | {detail}
                   </p>
                 </div>
-                <span>-{price}</span>
+                <span>- {price}</span>
               </li>
             ))}
           </ul>
         </div>
+
         <div className="loved">
           <h2>Most loved mocktails:</h2>
+
           <ul>
             {mockTailLists.map(({ name, country, detail, price }) => (
               <li key={name}>
@@ -59,7 +64,7 @@ const Cocktails = () => {
                     {country} | {detail}
                   </p>
                 </div>
-                <span>-{price}</span>
+                <span>- {price}</span>
               </li>
             ))}
           </ul>
